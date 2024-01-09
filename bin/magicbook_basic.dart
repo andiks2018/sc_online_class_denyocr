ca78609f29603b481dc867e9389e9188c4ca7835
andi <andiks2018@gmail.com>
2024-01-09 20:31:04 +0700

    first upload

---
+++ bin/magicbook_basic.dart
@@ -0,0 +1,13 @@
+import 'dev/run.dart';
+
+void main(List<String> arguments) {
+  String fullName = "andi";
+  String whatsapp = "081210430342";
+  String email = "andiks2018@gmail.com";
+
+  runChapters(
+    fullName: fullName,
+    email: email,
+    whatsapp: whatsapp,
+  );
+}
